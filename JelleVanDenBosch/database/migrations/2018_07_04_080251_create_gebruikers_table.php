8cb56a5c3326dc0d663d46050fa712ff1d568dc5
Jelle <jlbosch@hotmail.nl>
2018-07-04 10:06:55 +0200

    Tables added to migrations

--- JelleVanDenBosch/database/migrations/2018_07_04_080251_create_gebruikers_table.php
+++ JelleVanDenBosch/database/migrations/2018_07_04_080251_create_gebruikers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateGebruikersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('gebruikers', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('gebruiker')->unique();
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('wachtwoord');
+            $table->integer('admin')->default(0);
             $table->rememberToken();
-            $table->timestamps();
         });
     }
 
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('gebruikers');
     }
 }
